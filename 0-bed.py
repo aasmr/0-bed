11a8243fc693bc5d1e42c4490a6eed8191863f1f
Aleksey Smirnov <aasmr@ya.ru>
2021-02-06 05:10:11 +0300

    0.9 beta
    Тело написано, осталось украсить

--- 0-bed.py
+++ 0-bed.py
@@ -1,14 +1,52 @@
 import requests, sys
 import gui
 from PySide6.QtWidgets import QApplication
-res=requests.get('https://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new')
-res_seq=res.content.decode('utf-8').split('\n')
+from PySide6.QtCore import Slot, Signal, QObject
 
-res_seq=[int(i) for i in res_seq[:-1]]
-print(res_seq)
+@Slot(list, list)
+class Signals(QObject):
+    updateTable = Signal(list)
+    def __init__(self):
+        super().__init__()    
 
-app=QApplication(sys.argv)
-window=gui.MainWindow()
-window.show()
-app.exec_()
+class Body():
+    def __init__(self):
+        self.sg=Signals()
+        self.app=QApplication(sys.argv)
+        self.window=gui.MainWindow(self)
+        self.window.sg.lists.connect(self.get_rest)
+        
+            
+    def get_rest(self, name, req_count):
+        counter=[0 for i in range(len(name))]
+        max=0
+        cnt=1
+        cnt_=0
+        lot_dict={}
+        
+        for i in req_count:
+            max=max+i
+            if i ==1:
+                lot_dict[cnt]=name[cnt_]
+                cnt+=1
+            else:
+                for l in range(cnt, cnt+i):
+                    lot_dict[l]=name[cnt_]
+                cnt+=i
+            cnt_+=1
+        while (3 in counter) != True:
+            rand=self.req(1, 1, max)
+            counter[name.index(lot_dict[rand])]+=1
+            print(counter)
+            self.sg.updateTable.emit(counter)
+    
+    def req(self, num, min, max):
+        res=requests.get('https://www.random.org/integers/?num='+str(num)+'&min='+str(min)+'&max='+str(max)+'&col=1&base=10&format=plain&rnd=new')
+        res_seq=res.content.decode('utf-8').split('\n')
+        res_seq=[int(i) for i in res_seq[:-1]][0]
+        return res_seq
 
+if __name__=='__main__':
+    app=Body()
+    app.window.show()
+    app.app.exec_()
--- gui.py
+++ gui.py
@@ -4,42 +4,114 @@ Created on 5 фев. 2021 г.
 
 @author: smirnov_aa
 '''
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QTableWidget, QSplitter,\
-    QLineEdit, QPushButton
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QTableView,\
+    QLineEdit, QPushButton, QFrame, QHeaderView
+from PySide6.QtCore import QAbstractTableModel, Qt, QModelIndex, QObject
+from PySide6.QtCore import Signal, Slot
 
+class Sig(QObject):
+    lists=Signal(list, list)
+    def __init__(self):
+        super().__init__()      
 
 class MainWindow(QWidget):
     '''
     classdocs
     '''
-
-    def __init__(self):
+    def __init__(self, parent):
         super(MainWindow, self).__init__()
-        #self.setMinimumSize(240,320)
-        #self.setMaximumSize(240,320)
-        self._width=240
+        self.setMinimumSize(360,320)
+        self.setMaximumSize(360,320)
+        self._width=360
         self._height=320
         self.resize(self._width, self._height)
         self.setWindowTitle('0-бед or 0-bet')
         self.VBox=QVBoxLayout(self)
-        self.VBox.setContentsMargins(5, 5, 5, 33)
-        self.VBox.addStretch(1)
+        self.VBox.setContentsMargins(5, 5, 5, 5)
         self.restText=QLabel(self)
         self.restText.setText("Список заведений: ")
-        self.restTable=QTableWidget(self)
-        self.sep1=QSplitter(self)
-        self.restUnit=QLineEdit(self)
+        self.restTable=QTableView(self)
+        self.sep1 = QFrame(self)
+        self.sep1.setFrameShape(QFrame.HLine)
+        self.sep1.setFrameShadow(QFrame.Sunken)
+        self.restUnitName=QLineEdit(self)
         self.restAddButt=QPushButton(self)
         self.restAddButt.setText('Добавить заведение')
-        self.sep2=QSplitter(self)
-        self.restLottoreyButt=QPushButton(self)
-        self.restLottoreyButt.setText('Лотерея')
+        self.sep2 = QFrame(self)
+        self.sep2.setFrameShape(QFrame.HLine)
+        self.sep2.setFrameShadow(QFrame.Sunken)
+        self.restLotteryButt=QPushButton(self)
+        self.restLotteryButt.setText('Лотерея')
+        self.hashLabel=QLabel(self)
+        self.hashLabel.setText("Здесь будет хэш")
         
         self.VBox.addWidget(self.restText)
         self.VBox.addWidget(self.restTable)
         self.VBox.addWidget(self.sep1)
-        self.VBox.addWidget(self.restUnit)
+        self.VBox.addWidget(self.restUnitName)
         self.VBox.addWidget(self.restAddButt)
         self.VBox.addWidget(self.sep2)
-        self.VBox.addWidget(self.restLottoreyButt)
-        self.setLayout(self.VBox)   
+        self.VBox.addWidget(self.restLotteryButt)
+        self.VBox.addWidget(self.hashLabel)
+        self.setLayout(self.VBox)
+        
+        self.restAddButt.clicked.connect(self.addRestItem)
+        self.restLotteryButt.clicked.connect(self.startLottery)
+        
+        self.name_list=[]
+        self.reqCount_list=[]
+        self.sg=Sig()
+        parent.sg.updateTable.connect(self.updTbl)      
+        
+        
+    def addRestItem(self):
+            name=self.restUnitName.text()
+            if name in self.name_list:
+                self.reqCount_list[self.name_list.index(name)]=self.reqCount_list[self.name_list.index(name)]+1
+            else:
+                self.name_list.append(name)
+                self.reqCount_list.append(1)
+            table_model=RestaurantTableModeL(self.name_list, self.reqCount_list)
+            self.restTable.setModel(table_model)
+            self.restTable.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+            self.restTable.resizeColumnsToContents()
+            self.restTable.horizontalHeader().setStretchLastSection(True)
+              
+    def startLottery(self):
+        self.sg.lists.emit(self.name_list, self.reqCount_list)    
+    
+    @Slot(list)
+    def updTbl(self, cnt):
+        table_model=RestaurantTableModeL(self.name_list, self.reqCount_list, cnt)
+        self.restTable.setModel(table_model)
+        self.restTable.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.restTable.resizeColumnsToContents()
+        self.restTable.horizontalHeader().setStretchLastSection(True)
+            
+class RestaurantTableModeL(QAbstractTableModel):
+    def __init__(self, name_list, reqCount_list, counter=None):
+        super().__init__()
+        self.name_list=name_list
+        self.reqCount_list=reqCount_list
+        self.counter=counter
+    def rowCount(self, parent):
+        return len(self.name_list)
+    def columnCount(self, parent):
+        return 3
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            if col == 0:
+                return "Название заведения"
+            if col == 1:
+                return "Количество заявок"
+            if col == 2:
+                return "Результат"
+    def data(self, idx = QModelIndex(), role=None):
+        c=idx.column()
+        r=idx.row()
+        if c == 0 and role == Qt.DisplayRole:
+            return self.name_list[r]
+        if c == 1 and role == Qt.DisplayRole:
+            return self.reqCount_list[r]
+        if c == 2 and self.counter != None and role == Qt.DisplayRole:
+            return '+'*self.counter[r]
